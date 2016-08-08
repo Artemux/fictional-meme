d008930e85595d25d2a1f1ebdafe29948a7b2ed0
Artem <gm.88@inbox.ru>
2016-08-08 14:52:43 +0300

    event.js wrapped in anonumous function

--- stb/mag/js_lib/event.js
+++ stb/mag/js_lib/event.js
@@ -1,31 +1,33 @@
-var observerService;
-var stbEvent=
-{
-    onEvent : function(data){},
-    event : 0
-};
+(function() {
+    var observerService;
+    var stbEvent=
+    {
+        onEvent : function(data){},
+        event : 0
+    };
 
-//!!!
-// Для проекта на базе WebKit можно исключить все НИЖЕ следующее
-var myObserver = 
-{
-    observe : function(subject, topic, data)  
-        {
-        if (topic == "TeletecSTB")
+    //!!!
+    // пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅ WebKit пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
+    var myObserver =
+    {
+        observe : function(subject, topic, data)
             {
-                stbEvent.event = data;
-                stbEvent.onEvent(data);
-        }
-}
-};
-function initEvents(){
-  try
-  {
-    netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
-    observerService = Components.classes["@mozilla.org/observer-service;1"].getService(Components.interfaces.nsIObserverService);
-    observerService.addObserver(myObserver, "TeletecSTB", false);
-  }catch(e)
-  {
-  }
+            if (topic == "TeletecSTB")
+                {
+                    stbEvent.event = data;
+                    stbEvent.onEvent(data);
+            }
+    }
+    };
+    function initEvents(){
+      try
+      {
+        netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
+        observerService = Components.classes["@mozilla.org/observer-service;1"].getService(Components.interfaces.nsIObserverService);
+        observerService.addObserver(myObserver, "TeletecSTB", false);
+      }catch(e)
+      {
+      }
 
-}
+    }
+})();
